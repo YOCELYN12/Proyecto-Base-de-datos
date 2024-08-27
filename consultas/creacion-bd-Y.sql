e51f19684013d3272e9ea85ff7175a402c00f1be
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-27 08:21:28 -0600

    CREAR TABLA HABITACIONES

--- consultas/creacion-bd-Y.sql
+++ consultas/creacion-bd-Y.sql
@@ -11,7 +11,7 @@
 
 CREATE DATABASE Hotelogix;
 
-USE Hoteles;
+USE Hotelogix;
 
 CREATE TABLE Usuarios (Num_cedula int PRIMARY key, Nombre VARCHAR(100), Correo_electronico VARCHAR(255));
 SELECT * FROM Usuarios
@@ -19,7 +19,11 @@ SELECT * FROM Usuarios
 
  CREATE TABLE Reservas (Numero_reservacion INT PRIMARY KEY, Num_cedula INT, Fecha_reservacion DATE NOT NULL, Tiempo_reservacion TIME NOT NULL, CONSTRAINT Num_cedula Foreign Key (Num_cedula) REFERENCES Usuarios(Num_cedula))
 
-SELECT * FROM Reservas
+SELECT * FROM Reservas;
+
+
+CREATE TABLE Habitaciones (Numero_habitacion VARCHAR(100) PRIMARY KEY, Num_cedula Int, Numero_reservacion Int, Disponibilidad_habitaciones VARCHAR(100), Tipo_habitacion VARCHAR(255), FOREIGN KEY(Num_cedula) REFERENCES Usuarios(Num_cedula), CONSTRAINT Numero_reservacion Foreign Key (Numero_reservacion) REFERENCES Reservas(Numero_reservacion))
+
 
 -- CREATE TABLE Administradores(ID INT PRIMARY KEY, Nombre VARCHAR(100), correo_electronico varchar(100), CONSTRAINT cédula_jurídica FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
 -- CREATE TABLE Hoteles
