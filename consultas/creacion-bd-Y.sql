0095eff128b903ddfa129fd6ff9261e14d96313c
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-26 22:45:14 -0600

    TABLA USUARIOS

--- consultas/creacion-bd-Y.sql
+++ consultas/creacion-bd-Y.sql
@@ -10,3 +10,13 @@
 
 
 CREATE DATABASE Hotelogix;
+
+USE Hoteles;
+
+CREATE TABLE Usuarios (Num_cedula int PRIMARY key, Nombre VARCHAR(100), Correo_electronico VARCHAR(255));
+SELECT * FROM Usuarios
+
+
+
+-- CREATE TABLE Administradores(ID INT PRIMARY KEY, Nombre VARCHAR(100), correo_electronico varchar(100), CONSTRAINT cédula_jurídica FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
+-- CREATE TABLE Hoteles
