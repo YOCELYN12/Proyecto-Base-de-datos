c1951c749d0d5ed1829bd71f7beb71f8409de2f1
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-26 23:01:40 -0600

    CREAR TABLA RESERVAS

--- consultas/creacion-bd-Y.sql
+++ consultas/creacion-bd-Y.sql
@@ -17,6 +17,9 @@ CREATE TABLE Usuarios (Num_cedula int PRIMARY key, Nombre VARCHAR(100), Correo_e
 SELECT * FROM Usuarios
 
 
+ CREATE TABLE Reservas (Numero_reservacion INT PRIMARY KEY, Num_cedula INT, Fecha_reservacion DATE NOT NULL, Tiempo_reservacion TIME NOT NULL, CONSTRAINT Num_cedula Foreign Key (Num_cedula) REFERENCES Usuarios(Num_cedula))
+
+SELECT * FROM Reservas
 
 -- CREATE TABLE Administradores(ID INT PRIMARY KEY, Nombre VARCHAR(100), correo_electronico varchar(100), CONSTRAINT cédula_jurídica FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
 -- CREATE TABLE Hoteles
