dca4f1fc70c47b66ec9886a3a00c029570462e34
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-27 13:14:19 -0600

    Camnio en el valor Tiempo_reservacion de la tabla Reservas

--- consultas/creacion-bd-Y.sql
+++ consultas/creacion-bd-Y.sql
@@ -20,6 +20,7 @@ ALTER TABLE Usuarios ADD COLUMN Apellido VARCHAR(100) NOT NULL;
  CREATE TABLE Reservas (Numero_reservacion INT PRIMARY KEY, Num_cedula INT, Fecha_reservacion DATE NOT NULL, Tiempo_reservacion TIME NOT NULL, CONSTRAINT Num_cedula Foreign Key (Num_cedula) REFERENCES Usuarios(Num_cedula))
 
 SELECT * FROM Reservas;
+ALTER table Reservas MODIFY COLUMN Tiempo_reservacion Int
 
 CREATE TABLE Habitaciones (Numero_habitacion VARCHAR(100) PRIMARY KEY, Num_cedula Int, Numero_reservacion Int, Disponibilidad_habitaciones VARCHAR(100), Tipo_habitacion VARCHAR(255), FOREIGN KEY(Num_cedula) REFERENCES Usuarios(Num_cedula), CONSTRAINT Numero_reservacion Foreign Key (Numero_reservacion) REFERENCES Reservas(Numero_reservacion))
 
@@ -41,3 +42,6 @@ SELECT * FROM Usuarios;
 
 INSERT INTO Usuarios (Num_cedula, Nombre, Correo_electronico, Apellido) VALUES (1310225, 'Jose', 'jjvargas@gmail.com', 'Vargas'), (102583785, 'Maria', 'marias@gmail.com', 'Sandoval') ;
 INSERT INTO Usuarios (Num_cedula, Nombre, Correo_electronico, Apellido) VALUES (102583785, 'Maria', 'marias@gmail.com', 'Sandoval'), (302700071, 'Luis', 'Imhernandez@gmail.com', 'Hernandez'), (113310637, 'Marta', 'mquiros@gmail.com', 'Quiros'), (112750278,'Ester', 'ester@gmail.com','Ramirez'), (11241987, 'Marcos', 'mm@gmail.com','Barrientos'), (120430162, 'Alexander', 'amena@gmail.com', 'Mena')
+
+SELECT * FROM Reservas;
+SELECT * FROM  Usuarios;
