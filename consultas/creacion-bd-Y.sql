319226812b5b6ca3541cd69e1cdf62a4a1c834c3
YOCELYN12 <yocelynrivera9@gmail.com>
2024-08-27 08:41:14 -0600

    AGREGAR EL APELLIDO A LA TABLA USUARIOS

--- consultas/creacion-bd-Y.sql
+++ consultas/creacion-bd-Y.sql
@@ -2,9 +2,11 @@
 
 -- Create Table Hoteles(Cedula_Juridica INT PRIMARY KEY, Numero_habitacion INT, Numero_reservacion INT, Ubicacion VARCHAR(255), Tarifas INT, Nombre VARCHAR(255));
 
+-- CREATE TABLE Administradores(ID INT PRIMARY KEY, Nombre VARCHAR(100), correo_electronico varchar(100), CONSTRAINT cédula_jurídica FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
+-- CREATE TABLE Hoteles
+
 -- SELECT * FROM Hoteles
 
--- drop 
 
 
 
@@ -16,14 +18,12 @@ USE Hotelogix;
 CREATE TABLE Usuarios (Num_cedula int PRIMARY key, Nombre VARCHAR(100), Correo_electronico VARCHAR(255));
 SELECT * FROM Usuarios
 
+ALTER TABLE Usuarios ADD COLUMN Apellido VARCHAR(100) NOT NULL;
 
  CREATE TABLE Reservas (Numero_reservacion INT PRIMARY KEY, Num_cedula INT, Fecha_reservacion DATE NOT NULL, Tiempo_reservacion TIME NOT NULL, CONSTRAINT Num_cedula Foreign Key (Num_cedula) REFERENCES Usuarios(Num_cedula))
 
 SELECT * FROM Reservas;
 
-
 CREATE TABLE Habitaciones (Numero_habitacion VARCHAR(100) PRIMARY KEY, Num_cedula Int, Numero_reservacion Int, Disponibilidad_habitaciones VARCHAR(100), Tipo_habitacion VARCHAR(255), FOREIGN KEY(Num_cedula) REFERENCES Usuarios(Num_cedula), CONSTRAINT Numero_reservacion Foreign Key (Numero_reservacion) REFERENCES Reservas(Numero_reservacion))
 
-
--- CREATE TABLE Administradores(ID INT PRIMARY KEY, Nombre VARCHAR(100), correo_electronico varchar(100), CONSTRAINT cédula_jurídica FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
--- CREATE TABLE Hoteles
+SELECT * FROM Habitaciones
