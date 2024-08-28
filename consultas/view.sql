a085219e99c75d7105b9d0124f138ebfbeff3603
Naomy25sandi <naomysandi25@gmail.com>
2024-08-28 13:54:41 -0600

    creacion-view

--- consultas/view.sql
+++ consultas/view.sql
@@ -1,6 +1,8 @@
 --Crear una vista que muestre un reporte de las reservas realizadas en el día actual.
 
 CREATE VIEW reservas_realizadas AS
-SELECT , column2, ...
-FROM table_name
-WHERE condition;
+SELECT Fecha_reservacion, Tiempo_reservacion
+FROM Reservas
+WHERE Fecha_reservacion = CURDATE();
+
+SELECT * FROM reservas_realizadas;
