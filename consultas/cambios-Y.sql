841d24652fc52f9ab4650b6e325f9046f004037f
YOCELYN12 <yocelynrivera9@gmail.com>
2024-09-01 01:17:33 -0600

    CAMBIOS EN LA INFO DE LAS TABLAS

--- consultas/cambios-Y.sql
+++ consultas/cambios-Y.sql
@@ -8,24 +8,22 @@ CREATE DATABASE Hotelogix;
 USE Hotelogix;
 
 -- Creacion de todas las tablas, con sus llaves foraneas
-CREATE TABLE Clientes(ID_clientes INT AUTO_INCREMENT PRIMARY KEY, Num_cedula VARCHAR(20), Fecha_reservacion DATE, Dias_reservados INT, Nombre_hotel VARCHAR(30));
-CREATE TABLE Administradores(ID_admin INT AUTO_INCREMENT PRIMARY KEY, Nombre varchar(20), Apellido VARCHAR(20), Email VARCHAR(30));
-CREATE TABLE Usuarios (ID_usuario int PRIMARY KEY, Nombre VARCHAR(100), Apellido VARCHAR(100), Correo_electronico VARCHAR(255), ID_cliente INT, ID_Admin INT,FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_clientes), FOREIGN KEY (ID_Admin) REFERENCES Administradores(ID_Admin));
-CREATE TABLE Reservas (Numero_reservacion INT PRIMARY KEY, Fecha_reservacion DATE NOt NULL, Tiempo_reservacion INT NOT NULL, ID_usuario INT, Foreign Key (ID_usuario) REFERENCES Usuarios(ID_usuario));
-CREATE TABLE Hoteles (Cedula_juridica VARCHAR(100) PRIMARY KEY, Ubicacion VARCHAR(255), Tarifas INT, Nombre_hotel VARCHAR(255), Numero_reservacion INT, FOREIGN KEY (Numero_reservacion) REFERENCES Reservas(Numero_reservacion));
-CREATE TABLE Habitaciones (Numero_habitacion VARCHAR(100) PRIMARY KEY, cedula_juridica varchar(100), Numero_reservacion Int, Disponibilidad_habitaciones BOOLEAN, Tipo_habitacion VARCHAR(255), FOREIGN KEY(Numero_reservacion) REFERENCES Reservas(Numero_reservacion));
+CREATE TABLE Clientes(ID_clientes INT AUTO_INCREMENT PRIMARY KEY, Num_cedula VARCHAR(20), Fecha_reservacion DATE, Dias_reservados INT);
+CREATE TABLE Administradores(ID_admin INT AUTO_INCREMENT PRIMARY KEY, Nombre varchar(100), Apellido VARCHAR(100), Email VARCHAR(100));
+CREATE TABLE Usuarios (ID_usuario int AUTO_INCREMENT PRIMARY KEY, Nombre VARCHAR(100), Apellido VARCHAR(100), Correo_electronico VARCHAR(255), ID_cliente INT, ID_Admin INT,FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_clientes), FOREIGN KEY (ID_Admin) REFERENCES Administradores(ID_Admin));
+CREATE TABLE Hoteles (Cedula_juridica VARCHAR(100) PRIMARY KEY, Ubicacion VARCHAR(255), Tarifas INT, Nombre_hotel VARCHAR(255));
+CREATE TABLE Habitaciones (Numero_habitacion  INT AUTO_INCREMENT PRIMARY KEY, Cedula_juridica varchar(100), Disponibilidad_habitaciones BOOLEAN, Tipo_habitacion VARCHAR(255), FOREIGN KEY (cedula_juridica) REFERENCES Hoteles(cedula_juridica));
+CREATE TABLE Reservas (Numero_reservacion INT AUTO_INCREMENT PRIMARY KEY, Fecha_reservacion DATE NOT NULL, Tiempo_reservacion INT NOT NULL, Numero_habitacion INT, ID_usuario INT, FOREIGN KEY (Numero_habitacion) REFERENCES Habitaciones(Numero_habitacion), Foreign Key (ID_usuario) REFERENCES Usuarios(ID_usuario));
 
+j
 
-DROP Table habitaciones;
-DROP TABLE Reservas;
-DROP TABLE hoteles;
+--  DROP Table habitaciones;
+--  DROP TABLE Reservas;
+-- DROP TABLE hoteles;
 
-
-
-
-
-
-
+-- DROP TABLE clientes;
+drop TABLE usuarios;
+-- DROP TABLE administradores;
 
 
 -- Consultas
@@ -40,45 +38,54 @@ SELECT * FROM hoteles;
 
 -- INGRESO DE DATOS A LAS TABLAS 
 
-INSERT INTO Clientes( Num_cedula, Fecha_reservacion, Dias_reservados, Nombre_hotel) 
-VALUES ("3-0270-0071", "2023-12-25", 3, "Wyndham Escazu");
-INSERT INTO Clientes( Num_cedula, Fecha_reservacion, Dias_reservados, Nombre_hotel)
- VALUES ("1-1331-0637", "2023-09-8", 4, "Escazu Resident"),
-("1-310-225", "2023-12-20", 3, "Monte Real");
+INSERT INTO Clientes(Num_cedula, Fecha_reservacion, Dias_reservados) 
+VALUES ("3-0270-0071", "2023-12-25", 3),
+("1-0258-3785", "2023-10-15", 2),
+("1-12750278", "2023-09-8", 1),
+("1-1241-987", "2023-10-22", 2),
+("1-2043-0162", "2023-02-02", 4),
+("1-1331-0637", "2023-09-8", 4),
+("1-310-225", "2023-12-20", 3),
+("1-1331-0637", "2023-09-8", 4),
+("1-310-225", "2023-12-20", 3);
 
-INSERT INTO Clientes(Num_cedula, Fecha_reservacion, Dias_reservados, Nombre_hotel)
- VALUES ("1-0258-3785", "2023-10-15", 2, "Terrazas del Caribe"),
-("1-12750278", "2023-09-8", 1, "Sheraton"),
-(" 1-1241-987", "2023-10-22", 2, "Wyndham Tamarindo"),
-("1-2043-0162", "2023-02-02", 4, "Jardin de Lourdes");
 
-INSERT INTO Clientes( Num_cedula, Fecha_reservacion, Dias_reservados, Nombre_hotel)
- VALUES ("1-1331-0637", "2023-09-8", 4, "Escazu Resident"),
-("1-310-225", "2023-12-20", 3, "Monte Real");
+
 
 INSERT INTO Administradores(Nombre, Apellido, Email) 
-VALUES ("Javier", "Solis", "jsolis@gmail.com"); 
-INSERT INTO Administradores( Nombre, Apellido, Email) VALUES ("Jessica", "Madriz", "jmadriz@gmail.com");
+VALUES ("Javier", "Solis", "jsolis@gmail.com"),
+("Jessica", "Madriz", "jmadriz@gmail.com");
 
-INSERT INTO Usuarios(ID_usuario,Nombre, Apellido,Correo_electronico, ID_cliente, ID_Admin)
- VALUES (2,"Maria", "Sandoval","marias@gmail.com",4,1),
- (3,"Luis", "Hernandez", "Imhernandez@gmail.com",1,1),
- (4,"Marta","Quiros", "mquiros@gmail.com",2,1),
- (5,"Ester", "Ramirez", "ester@gmail.com",5,1),
- (6, "Marcos", "Barrientos", "mm@gmail.com",6,1),
- (7,"Alexander", "Mena", "amena@gmail.com",7,1),
- (8,"Javier", "Solis", "jsolis@gmail.com",6,1);
 
-INSERT INTO Administradores(Nombre, Apellido, Email) 
-VALUES ("Javier", "Solis", "jsolis@gmail.com"); 
 
-INSERT INTO Habitaciones (Numero_habitacion, cedula_juridica, Numero_reservacion, Disponibilidad_habitaciones, Tipo_habitacion)
-VALUES ("22", "200-8970-3967",02, "0", "Habitacion doble deluxe"),
-("11", "101-2569-9874", 08,"1" ,"Habitacion doble deluxe"),
-("10", "251-338-5987", 12,"0" ,"Habitacion sencilla"),
-("15", "222-338-5978", 45, "0", "Habitacion Deluxe"),
-("20","200-330-6678", 5, "1","Habitacion Deluxe"),
-("1","111-4240-654", 2, "0", "Habitacion sencilla");
+
+INSERT INTO Usuarios(Nombre, Apellido,Correo_electronico, ID_cliente, ID_Admin)
+ VALUES ("Maria", "Sandoval","marias@gmail.com",4,1),
+ ("Luis", "Hernandez", "Imhernandez@gmail.com",1,1),
+ ("Marta","Quiros", "mquiros@gmail.com",2,1),
+ ("Ester", "Ramirez", "ester@gmail.com",5,1),
+ ("Marcos", "Barrientos", "mm@gmail.com",6,1),
+ ("Alexander", "Mena", "amena@gmail.com",7,1),
+ ("Javier", "Solis", "jsolis@gmail.com",6,1);
+
+
+INSERT INTO Hoteles (Cedula_juridica, Ubicacion, Tarifas, Nombre_hotel) 
+VALUES ('200-8970-3967','Entrada Hotel Azania 450 metros directo manteniendo la derecha ',60000, 'Terrazas del Caribe Hotel'),
+('101-2569-9874', 'Cartago Cartago Costa Rica', 15000,'Dreams Lodge Orosi'),
+('251-338-5987', 'Calle Los Cedros, 10202 San Rafael Costa Rica',75000, 'Escazu Resident'),
+('222-338-5978', 'Route 27 350 mts West Multiplaza  10203 San Jose Costa Rica',95000,'Sheraton San Jose Hoten,Costa Rica'),
+('200-330-6678','200 metro al este y 800 metros al norte del Banco Nacionaaal de Tamarindo 00011 Tamarindo Costa Rica',81000, 'Wyndham Tamarindo'),
+('111-4240-654', '2800 km al Sur del Restaurante Changai Piedades de Santa Ana',50000, 'Jardin de Lourdes');
+
+
+
+INSERT INTO Habitaciones (Disponibilidad_habitaciones,Tipo_habitacion,cedula_juridica)
+VALUES ("0", "Habitacion doble deluxe","200-8970-3967"),
+("1" ,"Habitacion doble deluxe","101-2569-9874"),
+("0" ,"Habitacion sencilla","251-338-5987"),
+("0", "Habitacion Deluxe","222-338-5978"),
+("1","Habitacion Deluxe","200-330-6678"),
+("0", "Habitacion sencilla","111-4240-654");
 
 
 
--- consultas/store_procedure.sql
+++ consultas/store_procedure.sql
@@ -5,18 +5,19 @@
 
 
 --Store Procedures para poder agregar reservas.
-CREATE PROCEDURE AGREGAR_RESERVA(IN P_Num_reservacion INT, IN P_date DATE, IN P_time INT, IN P_ID INT)
+CREATE PROCEDURE AGREGAR_RESERVA( IN P_date DATE, IN P_time INT, IN P_Numero_habitacion INT, IN P_ID INT)
 BEGIN 
-    INSERT INTO Reservas(Numero_reservacion, Fecha_reservacion, Tiempo_reservacion, ID_usuario) VALUES (P_Num_reservacion, P_date, P_time, P_ID);
+    INSERT INTO Reservas(Fecha_reservacion,Tiempo_reservacion, Numero_habitacion, ID_usuario) VALUES (P_date, P_time,P_Numero_habitacion, P_ID);
 END ;
 
 -- Se llama el Store procedures para poder ingregar los datos
-Call AGREGAR_RESERVA(02, "2023-10-25", 2,2);
-CALL AGREGAR_RESERVA(08,"2023-10-10",1,3);
-CALL AGREGAR_RESERVA(12,"2023-09-08",4,4);
-CALL AGREGAR_RESERVA(45, "2023-09-08",1,5);
-CALL AGREGAR_RESERVA(05, "2023-10-22",2,6);
-CALL AGREGAR_RESERVA(10, "2023-02-02",2,7);
+Call AGREGAR_RESERVA("2023-10-25",2,1,2);
+CALL AGREGAR_RESERVA("2023-10-10",1,2,3);
+CALL AGREGAR_RESERVA("2023-09-08",4,3,4);
+CALL AGREGAR_RESERVA("2023-09-08",1,4,1);
+CALL AGREGAR_RESERVA("2023-10-22",2,5,6);
+CALL AGREGAR_RESERVA("2023-02-02",2,6,5);
+
 
 
 -- DROP PROCEDURE `AGREGAR_RESERVA`
@@ -36,4 +37,4 @@ CALL DISPONIBILIDAD_HABITACIONES("2024-07-25")
 SELECT * FROM reservas
 
 -- Para borrar el store procedures
--- DROP PROCEDURE DISPONIBILIDAD_HABITACIONES;
+DROP PROCEDURE DISPONIBILIDAD_HABITACIONES;
